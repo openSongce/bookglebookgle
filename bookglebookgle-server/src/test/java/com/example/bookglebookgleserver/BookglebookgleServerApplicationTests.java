dc08bfad67dfe53e12144a1b8140d6d0baf36cac
heojimyeong <wlaud0823@naver.com>
2025-07-21 10:03:36 +0900

    chore : Spring Boot 프로젝트 생성

---
+++ bookglebookgle-server/src/test/java/com/example/bookglebookgleserver/BookglebookgleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookglebookgleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookglebookgleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
