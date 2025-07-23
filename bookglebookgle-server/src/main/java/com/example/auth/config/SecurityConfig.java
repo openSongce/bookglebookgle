5e2a76bc159e523c8643835a172a7d7b525c8324
DESKTOP-DLOPF19\SSAFY <eunsolhh@gmail.com>
2025-07-23 12:19:04 +0900

    git commit -m "S13P11D204-210 <chore: spring과 DB연동 수정>"

--- bookglebookgle-server/src/main/java/com/example/auth/config/SecurityConfig.java
+++ bookglebookgle-server/src/main/java/com/example/auth/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
+
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
