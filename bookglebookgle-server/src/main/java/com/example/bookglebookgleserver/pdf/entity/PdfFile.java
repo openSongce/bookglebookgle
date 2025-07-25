cd87c31b17cdbd2d933de920fd7098529690785c
heojimyeong <wlaud0823@naver.com>
2025-07-25 13:07:18 +0900

    S13P11D204-73 feat: pdf-file entity 및 repository 생성

---
+++ bookglebookgle-server/src/main/java/com/example/bookglebookgleserver/pdf/entity/PdfFile.java
@@ -0,0 +1,4 @@
+package com.example.bookglebookgleserver.pdf.entity;
+
+public class PdfFile {
+}
---
+++ bookglebookgle-server/src/main/java/com/example/bookglebookgleserver/pdf/repository/PdfFileRepository.java
@@ -0,0 +1,4 @@
+package com.example.bookglebookgleserver.pdf.repository;
+
+public interface PdfFileRepository {
+}
