eb168c97a7de5a639d37584eb0598ca833665a71
heojimyeong <wlaud0823@naver.com>
2025-07-28 10:35:41 +0900

    feat : GlobalExceptionHandler 생성

---
+++ bookglebookgle-server/src/main/java/com/example/bookglebookgleserver/global/exception/AuthException.java
@@ -0,0 +1,7 @@
+package com.example.bookglebookgleserver.global.exception;
+
+public class AuthException extends RuntimeException {
+    public AuthException(String message) {
+        super(message);
+    }
+}
---
+++ bookglebookgle-server/src/main/java/com/example/bookglebookgleserver/global/exception/dto/ErrorResponse.java
@@ -0,0 +1,3 @@
+package com.example.bookglebookgleserver.global.exception.dto;
+
+public record ErrorResponse(String code, String message) { }
