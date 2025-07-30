2a19fa766d9276bfbd993ace9e2cb9d95cf0bccc
송진우 <rkddkwl059@naver.com>
2025-07-30 14:14:14 +0900

    fix(S13P11D204-58) : 로그인 실패시 꺼지는 현상 픽스

--- bookglebookgle-android/app/src/main/java/com/ssafy/bookglebookgle/usecase/KakaoLoginUseCase.kt
+++ bookglebookgle-android/app/src/main/java/com/ssafy/bookglebookgle/usecase/KakaoLoginUseCase.kt
@@ -1,5 +1,6 @@
 package com.ssafy.bookglebookgle.usecase
 
+import android.util.Log
 import com.ssafy.bookglebookgle.repository.LoginRepository
 import com.ssafy.bookglebookgle.util.TokenManager
 import com.ssafy.bookglebookgle.util.UserInfoManager
@@ -7,13 +8,17 @@ import javax.inject.Inject
 
 class KakaoLoginUseCase @Inject constructor(
     private val repository: LoginRepository,
-    private val tokenManager: TokenManager,
-    private val userInfoManager: UserInfoManager
+    private val tokenManager: TokenManager
 ) {
     suspend operator fun invoke(accessToken: String): Boolean {
-        val response = repository.loginWithKakao(accessToken)
-        tokenManager.saveTokens(response.accessToken, response.refreshToken)
-        userInfoManager.saveUserInfo(response)
-        return true
+        return try {
+            val response = repository.loginWithKakao(accessToken)
+            tokenManager.saveTokens(response.accessToken, response.refreshToken)
+            true
+        } catch (e: Exception) {
+            Log.e("KAKAO_LOGIN", "카카오 로그인 실패", e)
+            false
+        }
     }
+
 }
