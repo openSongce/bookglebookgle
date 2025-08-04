490a90f7b23922a3a25a4960645561022d895b6a
heojimyeong <wlaud0823@naver.com>
2025-08-04 11:01:11 +0900

    S13P11D204-100 feat: 사용자가 가입하지 않은 그룹만 반환하는 그룹 목록 조회 API 구현

--- bookglebookgle-server/src/main/java/com/example/bookglebookgleserver/group/service/GroupService.java
+++ bookglebookgle-server/src/main/java/com/example/bookglebookgleserver/group/service/GroupService.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface GroupService {
     GroupCreateResponseDto createGroup(GroupCreateRequestDto dto, MultipartFile pdfFile, User user);
     void createGroupWithoutOcr(GroupCreateRequestDto dto, MultipartFile pdfFile, User user);
-    List<GroupListResponseDto> getGroupList();
+    List<GroupListResponseDto> getNotJoinedGroupList(Long userId);
     GroupDetailResponse getGroupDetail(Long groupId, User user);
     ResponseEntity<Resource> getPdfFileResponse(Long groupId, User user);
     List<MyGroupSummaryDto> getMyGroupList(Long userId);
