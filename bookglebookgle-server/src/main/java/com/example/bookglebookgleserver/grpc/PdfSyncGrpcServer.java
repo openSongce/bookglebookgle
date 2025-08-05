56a7f54ef768b0a25f37116b612e4b1d7ce5bd58
송진우 <rkddkwl059@naver.com>
2025-08-05 10:35:55 +0900

    !hotfix(S13P11D204-256) : gRPC 서버 실행 오류 픽스

--- bookglebookgle-server/src/main/java/com/example/bookglebookgleserver/grpc/PdfSyncGrpcServer.java
+++ bookglebookgle-server/src/main/java/com/example/bookglebookgleserver/grpc/PdfSyncGrpcServer.java
@@ -5,13 +5,18 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 public class PdfSyncGrpcServer {
-    public static void main(String[] args) throws Exception {
-        Server server = ServerBuilder.forPort(6565)
-            .addService(new PdfSyncServiceImpl())
-            .build();
+    public static void main(String[] args) {
+        try {
+            Server server = ServerBuilder.forPort(6565)
+                .addService(new PdfSyncServiceImpl())
+                .build();
 
-        System.out.println("gRPC 서버 시작: 포트 6565");
-        server.start();
-        server.awaitTermination();
+            System.out.println("gRPC 서버 시작: 포트 6565");
+            server.start();
+            server.awaitTermination();
+        } catch (Exception e) {
+            System.err.println("gRPC 서버 실행 중 예외 발생: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
