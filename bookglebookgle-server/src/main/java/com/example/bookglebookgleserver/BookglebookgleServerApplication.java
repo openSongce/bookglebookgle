2503bc70eb5fced496da0e7fcd7a092ec9b62c2a
heojimyeong <wlaud0823@naver.com>
2025-08-05 21:03:59 +0900

    S13P11D204-131 feat : gRPC 서비스 Spring 통합 구조로 전환 및 PDF 동기화 서비스 등록

--- bookglebookgle-server/src/main/java/com/example/bookglebookgleserver/BookglebookgleServerApplication.java
+++ bookglebookgle-server/src/main/java/com/example/bookglebookgleserver/BookglebookgleServerApplication.java
@@ -3,10 +3,6 @@ package com.example.bookglebookgleserver;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import jakarta.annotation.PostConstruct;
-import org.springframework.stereotype.Component;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 @SpringBootApplication
@@ -26,27 +22,4 @@ public class BookglebookgleServerApplication {
         SpringApplication.run(BookglebookgleServerApplication.class, args);
 
     }
-    
-    @Component
-    public class GrpcServerLauncher {
-
-        private static final Logger log = LoggerFactory.getLogger(GrpcServerLauncher.class);
-
-        @PostConstruct
-        public void startGrpcServer() {
-            Thread grpcThread = new Thread(() -> {
-                try {
-                    log.info("gRPC 서버 초기화 중...");
-                    com.example.bookglebookgleserver.grpc.PdfSyncGrpcServer.main(null);
-                } catch (Exception e) {
-                    log.error("gRPC 서버 실행 중 예외 발생", e);
-                }
-            });
-
-            grpcThread.setDaemon(false);
-            grpcThread.start();
-        }
-    }
-
-
 }
