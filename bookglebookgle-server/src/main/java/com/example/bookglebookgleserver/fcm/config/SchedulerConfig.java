b0ca6e15d1b65a11bbf5b370492accbe4e178685
heojimyeong <wlaud0823@naver.com>
2025-08-11 10:44:10 +0900

    feat : FCM @EnableScheduling 추가

--- bookglebookgle-server/src/main/java/com/example/bookglebookgleserver/fcm/config/SchedulerConfig.java
+++ bookglebookgle-server/src/main/java/com/example/bookglebookgleserver/fcm/config/SchedulerConfig.java
@@ -2,8 +2,10 @@ package com.example.bookglebookgleserver.fcm.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+@EnableScheduling
 @Configuration
 public class SchedulerConfig {
     @Bean
